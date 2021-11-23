4d7809acba7aa3ea7729af9b2f200b8568229172
luchogimenez <gimenezluis001@gmail.com>
2021-11-23 17:42:18 -0300

    creacion de rama libro-sofi-luis

--- src/main/java/edu/sucho/libreriaweb/repository/LibroRepository.java
+++ src/main/java/edu/sucho/libreriaweb/repository/LibroRepository.java
@@ -1,5 +1,7 @@
 package edu.sucho.libreriaweb.repository;
 
+import edu.sucho.libreriaweb.model.Autor;
+import edu.sucho.libreriaweb.model.Editorial;
 import edu.sucho.libreriaweb.model.Libro;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -22,4 +24,17 @@ public interface LibroRepository extends BaseRepository<Libro, Integer> {
 
     @Query(value = "SELECT * FROM libro WHERE libro.titulo LIKE %:titulo%", nativeQuery = true)
     List<Libro> findByTitulo(@Param("titulo") String titulo);
+    
+    @Query(value = "CALL lsp_cambiar_estado_libro(:id,:alta);", nativeQuery = true)
+    String changeStatus(@Param("id") int id, @Param("alta") Boolean alta);
+    
+    @Query(value = "CALL lsp_modificar_libro(:id,:titulo,:isbn,:anio,"
+            + ":ejemplares,:ejemplaresPrestados,:ejemplaresRestantes,"
+            + ":autor,:editorial);", nativeQuery = true)
+    String updateEditorial(@Param("id") Integer id, @Param("titulo") String titulo,
+            @Param("isbn") Long isbn,@Param("anio") Integer anio,
+            @Param("ejemplares") Integer ejemplares,
+            @Param("ejemplaresPrestados") Integer ejemplaresPrestados,
+            @Param("ejemplaresRestantes") Integer ejemplaresRestantes,
+            @Param("autor") Autor autor,@Param("editorial") Editorial editorial);
 }
