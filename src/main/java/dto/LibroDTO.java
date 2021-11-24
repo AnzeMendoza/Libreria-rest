ec7b29a9b5b53624d01f7be24d64300edea9f40a
Luis <gimenezluis001@gmail.com>
2021-11-24 17:52:09 -0300

    Implementacion de clase Libro-DTO

---
+++ src/main/java/dto/LibroDTO.java
@@ -0,0 +1,19 @@
+package dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LibroDTO {
+    
+    private Integer id;
+    private String titulo;
+    private Long isbn;
+    private Integer anio;
+    private Integer ejemplares;
+    private Integer ejemplaresPrestados;
+    private Integer ejemplaresRestantes;
+    private Boolean alta;
+    private String autor;
+}
