e8d83b2c9aca021a20db71abadeaed915dd653e3
Facundo Sebastian Cordoba <cordobafs@gmail.com>
2021-11-25 12:17:57 -0300

    se realiza un refactor de los test unitarios para  el repositorio editoriales

--- src/test/java/edu/sucho/libreriaweb/repository/EditorialRepositoryTest.java
+++ src/test/java/edu/sucho/libreriaweb/repository/EditorialRepositoryTest.java
@@ -1,6 +1,9 @@
 package edu.sucho.libreriaweb.repository;
 
 import edu.sucho.libreriaweb.model.Editorial;
+import edu.sucho.libreriaweb.util.Comparacion;
+import edu.sucho.libreriaweb.util.Conexion;
+import edu.sucho.libreriaweb.util.Util;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +23,8 @@ class EditorialRepositoryTest {
     @Autowired
     EditorialRepository editorialRepository;
     static int id;
-
-
+    static Connection conexion;
+    static Comparacion<Editorial>comparacion;
 
     @BeforeAll
     public static void beforeAllTest() {
@@ -29,19 +32,25 @@ class EditorialRepositoryTest {
         System.out.println("para todos los test, sirve para declarar variables que se usan en todos los test");
         System.out.println("@BeforeAll --> beforeAllTest()");
     }
+
     @AfterAll
-    public static void afterAllTest(){
+    public static void afterAllTest() {
         System.out.println("para todos los test, sirve para liberar recursos");
         System.out.println("@afterAllTest() --> afterAllTest()");
     }
+
     @BeforeEach
     void setUp() {
-
+        conexion = Conexion.conect();
+        comparacion = new Comparacion<>();
         System.out.println("se ejecuta por cada Test");
         System.out.println("@BeforeEach --> setUp()");
     }
+
     @AfterEach
     void tearDown() {
+        Conexion.disconect(conexion);
+        comparacion = null;
         System.out.println("se ejecuta por cada Test");
         System.out.println("@AfterEach --> tearDown()");
     }
@@ -49,50 +58,51 @@ class EditorialRepositoryTest {
     @DisplayName("Validar Referencia No Nula EditorialRepository")
     @Test
     void editorialRepositoryNotNullTest() {
-        assertNotNull(editorialRepository,"la referencia al  repositorio editorial es  nula");
+        assertNotNull(editorialRepository, "la referencia al  repositorio editorial es  nula");
     }
+
     @DisplayName("Cambio de Estado Editorial")
     @Test
     void changeStatusTest() {
         String esperado = "OK";
-        String actual = editorialRepository.changeStatus(id,Boolean.TRUE);
+        String actual = editorialRepository.changeStatus(id, Boolean.TRUE);
         assertEquals(esperado, actual, "fallo el cambio de estado");
     }
 
-
     @DisplayName("Modificar Editorial")
     @Test
-     void updateEditorialTest(){
-        String esperado = "OK,"+id;
+    void updateEditorialTest() {
+        String esperado = "OK," + id;
         String actual = modificarEditorial(id);
-        assertEquals(esperado,actual,"ya existe  un editorial   con ese nombre");
-     }
+        assertEquals(esperado, actual, "ya existe  un editorial   con ese nombre");
+    }
+
     private String modificarEditorial(int id) {
         String nombreEditorial;
         String actual;
-        do{
-            nombreEditorial = UUID.randomUUID().toString().replace("-", "").substring(0,9);
-             actual = editorialRepository.updateEditorial(id,nombreEditorial);
-        }while(actual =="ya existe  un editorial   con ese nombre");
+        do {
+            nombreEditorial = UUID.randomUUID().toString().replace("-", "").substring(0, 9);
+            actual = editorialRepository.updateEditorial(id, nombreEditorial);
+        } while (actual == "ya existe  un editorial   con ese nombre");
         return actual;
     }
 
     @DisplayName("validar Editoriales activos")
-     @Test
-     void findAllByAltaTest() throws SQLException, ClassNotFoundException {
-        List<Editorial> esperado = editorialRepository.findAllByAlta();
+    @Test
+    void findAllByAltaTest() throws SQLException {
+        List<Editorial> esperado = Util.getEditoriales(conexion, "SELECT * FROM editorial WHERE editorial.alta = true");
         List<Editorial> actual = editorialRepository.findAllByAlta();
-        assertEquals(esperado, actual,"no son iguales los array");
-     }
+        assertTrue(comparacion.IsEqualsLists(esperado,actual), "los array no son iguales");
+    }
 
     @DisplayName("validar Editoriales por nombre ")
     @Test
-    void findByValueField() throws SQLException, ClassNotFoundException {
-        Editorial esperado = editorialRepository.findByValueField("nombre");
-        Editorial actual = editorialRepository.findByValueField("nombre");
-        assertEquals(esperado, actual,"no son los mismo editoriales");
+    void findByValueField() throws SQLException {
+        String nombre ="santillana";
+        Editorial esperado = Util.getEditoriales(conexion, "SELECT * FROM editorial WHERE editorial.nombre = \"santillana\" ").get(0);
+        Editorial actual = editorialRepository.findByValueField(nombre);
+        assertEquals(esperado, actual, "no son los mismo editoriales");
     }
-
 
 
 }
