fc0c5856380b227213927b0d7689c02bff1161dd
Facundo Sebastian Cordoba <cordobafs@gmail.com>
2021-11-25 12:17:16 -0300

    se realiza un refactor del getBoolean que recibe un entero y lo transforma a booleano y se agrego la clase getEditoriales para obtener todos los editoriales  . metodo usado en los test de editoriales

--- src/main/java/edu/sucho/libreriaweb/util/Util.java
+++ src/main/java/edu/sucho/libreriaweb/util/Util.java
@@ -1,18 +1,21 @@
 package edu.sucho.libreriaweb.util;
 
 import edu.sucho.libreriaweb.exception.ExceptionBadRequest;
+import edu.sucho.libreriaweb.model.Editorial;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
-
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Util {
 
     public static void ValidarParametros(BindingResult result) throws ExceptionBadRequest {
-
-            if (result.hasErrors()) { // Hay un error
+            if (result.hasErrors()) {
                 List<ObjectError> oEs = result.getAllErrors().stream().collect(Collectors.toList());
                 String err = "";
                 for (ObjectError oE : oEs) {
@@ -29,5 +32,23 @@ public class Util {
         } catch (NumberFormatException nfe){
             throw new ExceptionBadRequest("Debe ingresar una cadena con un numero");
         }
+    }
+
+    private static boolean getBoolean(int valor) {
+        return (valor==1);
+    }
+
+    public static List<Editorial> getEditoriales(Connection conexion, String query) throws SQLException {
+        ResultSet rs = Conexion.getResultSet(conexion,query);
+        List<Editorial> editoriales= new ArrayList<>();
+        while (Conexion.existeNext(rs))
+        {
+            Editorial editorial = new Editorial();
+            editorial.setId(rs.getInt("id"));
+            editorial.setAlta(getBoolean(rs.getInt("alta")));
+            editorial.setNombre(rs.getString("nombre"));
+            editoriales.add(editorial);
+        }
+        return editoriales;
     }
 }
