afabcf3505cc539467e3ccab5035dc9caf9f9d5d
Facundo Sebastian Cordoba <cordobafs@gmail.com>
2021-11-26 10:53:31 -0300

    se modifica la llamada al store procedure para crear  editorial en repository editorial

--- src/main/java/edu/sucho/libreriaweb/repository/EditorialRepository.java
+++ src/main/java/edu/sucho/libreriaweb/repository/EditorialRepository.java
@@ -15,7 +15,7 @@ public interface EditorialRepository extends BaseRepository<Editorial, Integer> 
     Editorial findByValueField(@Param("name")String name);
     @Query(value="CALL lsp_cambiar_estado_editorial(:id,:status);" , nativeQuery = true)
     String changeStatus(@Param("id")int id, @Param("status")Boolean estado);
-    @Query(value = "CALL lsp_alta_editorial(:name)", nativeQuery = true)
+    @Query(value = "CALL lsp_crear_editorial(:name)", nativeQuery = true)
     String saveEditorial(@Param("name")String name);
     @Query(value="CALL lsp_modificar_editorial(:id,:name);" , nativeQuery = true)
     String updateEditorial(@Param("id")Integer id, @Param("name")String name);
