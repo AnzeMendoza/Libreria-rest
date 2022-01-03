d39451e0cf3a1f43de28c8e7f2186ef18d276bff
Facundo cordoba <cordobafs@gmail.com>
2022-01-03 12:36:45 -0300

    crea clase UserNotAutorizer.java
    se crea la clase  PoJo UserNotAutorizer para  usar en el response http  cuando un usuario no este autorizado para acceder a los servicios

---
+++ src/main/java/edu/sucho/libreriaweb/model/dto/UserNotAutorizer.java
@@ -0,0 +1,16 @@
+package edu.sucho.libreriaweb.model.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+public class UserNotAutorizer {
+    private String  error;
+    private String  error_description;
+
+
+}
