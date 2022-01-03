1bc80db32aa7979669b63c913a96b444a00e6d0b
Luis Alberto Gimenez <lgimenez@serv.externos.prismamp.com>
2022-01-03 17:27:32 -0300

    implementacion (parcial) de spring-security

---
+++ src/main/java/edu/sucho/libreriaweb/model/entity/Rol.java
@@ -0,0 +1,19 @@
+package edu.sucho.libreriaweb.model.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Rol {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Byte Id;
+
+    @Column(length = 32,nullable = false,unique = true)
+    private String type;
+}
