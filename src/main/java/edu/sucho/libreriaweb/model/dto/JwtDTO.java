9a92ff1aea4316752c9c4fd6db01535fb6b84ab8
Facundo cordoba <cordobafs@gmail.com>
2022-01-03 12:30:33 -0300

    Modificacion JwtDTO
    Se agrega  anotacion @Component y  se elimina el constructor que recibia solo un parametro

--- src/main/java/edu/sucho/libreriaweb/model/dto/JwtDTO.java
+++ src/main/java/edu/sucho/libreriaweb/model/dto/JwtDTO.java
@@ -2,19 +2,17 @@ package edu.sucho.libreriaweb.model.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
-
+@Component
 @Getter
 @Setter
 public class JwtDTO {
-    private String date;
+    private String date = new Date().toString();
     private String jwt;
+    @Value("${security.jwt.ttlMillis}")
     private String ttl;
 
-    public JwtDTO(String jwt) {
-        date = new Date().toString();
-        this.jwt=jwt;
-        this.ttl = System.getenv().get("JWT_TTL");
-    }
 }
