acdcc9ca5d425af61d8b2c16cec42785362a2f22
Mirna Pamela Diaz <pamela_diaz_24@outlook.com>
2022-01-03 15:27:38 -0300

    se agregan contructores a la entidad JwtDTO

--- src/main/java/edu/sucho/libreriaweb/model/dto/JwtDTO.java
+++ src/main/java/edu/sucho/libreriaweb/model/dto/JwtDTO.java
@@ -1,6 +1,7 @@
 package edu.sucho.libreriaweb.model.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -9,10 +10,17 @@ import java.util.Date;
 @Component
 @Getter
 @Setter
+@NoArgsConstructor
 public class JwtDTO {
     private String date = new Date().toString();
     private String jwt;
     @Value("${security.jwt.ttlMillis}")
     private String ttl;
+
+    public JwtDTO(String jwt) {
+        this.date=new Date().toString();
+        this.jwt=jwt;
+    }
+
 
 }
