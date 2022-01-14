48f8744ced3b6b6548ca1a2e08d27610ed39c39a
hanzemendoza <hanzemendoza@est.frba.utn.edu.ar>
2022-01-14 12:44:12 -0300

    se refactorizo AutorController

--- src/main/java/edu/sucho/libreriaweb/controller/AutorController.java
+++ src/main/java/edu/sucho/libreriaweb/controller/AutorController.java
@@ -4,6 +4,7 @@ import edu.sucho.libreriaweb.config.ResponseInfo;
 import edu.sucho.libreriaweb.exception.ExceptionBBDD;
 import edu.sucho.libreriaweb.exception.ExceptionBadRequest;
 import edu.sucho.libreriaweb.model.dto.AutorRequestDTO;
+import edu.sucho.libreriaweb.model.entity.Autor;
 import edu.sucho.libreriaweb.model.mapper.ModelMapperDTO;
 import edu.sucho.libreriaweb.service.inter.AutorService;
 import edu.sucho.libreriaweb.util.Uri;
@@ -17,6 +18,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -32,24 +34,18 @@ public class AutorController {
     @GetMapping("/")
     public ResponseEntity<?> getAll() {
         try {
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(modelMapperDTO.listAutorToDto(autorService.findAll()));
-        } catch (ExceptionBBDD e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(new ResponseInfo(HttpStatus.BAD_REQUEST.value()
-                            , e.getMessage(), Uri.AUTOR));
+            return responseAutors(autorService.findAll());
+        } catch (ExceptionBBDD ebd) {
+            return responseExceptionBBDD(ebd, Uri.AUTOR);
         }
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getOne(@PathVariable("id") int id) {
         try {
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(modelMapperDTO.autorToDto(autorService.findById(id)));
-        } catch (ExceptionBBDD e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(new ResponseInfo(HttpStatus.BAD_REQUEST.value()
-                            , e.getMessage(), String.format("/%s/%d", Uri.AUTOR, id)));
+            return responseAutor(id);
+        } catch (ExceptionBBDD ebd) {
+            return responseExceptionBBDD(ebd, Uri.AUTOR + "/" + id);
         }
     }
 
@@ -59,11 +55,11 @@ public class AutorController {
                                   BindingResult result) throws ExceptionBadRequest {
         try {
             Util.ValidarParametros(result);
-            return ResponseEntity.status(HttpStatus.CREATED).body(autorService.save(autor));
-        } catch (ExceptionBBDD | ExceptionBadRequest ebd) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(new ResponseInfo(HttpStatus.BAD_REQUEST.value()
-                            , ebd.getMessage(), Uri.AUTOR));
+            return responseSaveAutor(autor);
+        } catch (ExceptionBBDD ebd) {
+            return responseExceptionBBDD(ebd, Uri.AUTOR);
+        } catch (ExceptionBadRequest ebr) {
+            return responseExceptionBadRequest(ebr, Uri.AUTOR);
         }
     }
 
@@ -74,39 +70,63 @@ public class AutorController {
                                     BindingResult result) throws ExceptionBadRequest {
         try {
             Util.ValidarParametros(result);
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(autorService.update(id, autor));
-
-        } catch (ExceptionBBDD | ExceptionBadRequest ebd) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(new ResponseInfo(HttpStatus.BAD_REQUEST.value()
-                            , ebd.getMessage(), String.format("/%s/%d", Uri.AUTOR, id)));
+            return responseAutorUpdate(id, autor);
+        } catch (ExceptionBBDD ebd) {
+            return responseExceptionBBDD(ebd, Uri.AUTOR + "/" + id);
+        } catch (ExceptionBadRequest ebr) {
+            return responseExceptionBadRequest(ebr, Uri.AUTOR + "/" + id);
         }
     }
 
     @GetMapping("/activar/{id}")
-//    @PreAuthorize("hasRole('ROLE_PERSONAL') OR hasRole('ROLE_ADMIN')")
-    private ResponseEntity<?> active(@PathVariable("id") int id) throws ExceptionBadRequest {
+    @PreAuthorize("hasRole('ROLE_PERSONAL') OR hasRole('ROLE_ADMIN')")
+    public ResponseEntity<?> active(@PathVariable("id") int id) {
         try {
-            return ResponseEntity.status(HttpStatus.OK).body(new ResponseInfo(HttpStatus.OK.value(), autorService.enableStatus(id), String.format("%s/%d", Uri.AUTOR_ACTIVAR, id)));
+            return responseAutorEnable(id);
         } catch (ExceptionBBDD ebd) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(new ResponseInfo(HttpStatus.BAD_REQUEST.value()
-                            , ebd.getMessage(), String.format("%s/%d", Uri.AUTOR_ACTIVAR, id)));
+            return responseExceptionBBDD(ebd, Uri.AUTOR_ACTIVAR + "/" + id);
         }
     }
 
     @GetMapping("/desactivar/{id}")
     @PreAuthorize("hasRole('ROLE_PERSONAL') OR hasRole('ROLE_ADMIN')")
-    private ResponseEntity<?> deactivate(@PathVariable("id") int id) throws ExceptionBadRequest {
+    public ResponseEntity<?> deactivate(@PathVariable("id") int id) {
         try {
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(new ResponseInfo(HttpStatus.OK.value()
-                            , autorService.disableStatus(id), String.format("%s/%d", Uri.AUTOR_DESACTIVAR, id)));
+            return responseAutorDisable(id);
         } catch (ExceptionBBDD ebd) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(new ResponseInfo(HttpStatus.BAD_REQUEST.value()
-                            , ebd.getMessage(), String.format("%s/%d", Uri.AUTOR_DESACTIVAR, id)));
+            return responseExceptionBBDD(ebd, Uri.AUTOR_DESACTIVAR + "/" + id);
         }
+    }
+
+    private ResponseEntity<?> responseAutors(List<Autor> autors) throws ExceptionBBDD {
+        return ResponseEntity.status(HttpStatus.OK).body(modelMapperDTO.listAutorToDto(autors));
+    }
+
+    private ResponseEntity<?> responseSaveAutor(AutorRequestDTO autor) throws ExceptionBBDD, ExceptionBadRequest {
+        return ResponseEntity.status(HttpStatus.CREATED).body(autorService.save(autor));
+    }
+
+    private ResponseEntity<?> responseAutorEnable(int id) throws ExceptionBBDD {
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseInfo(HttpStatus.OK.value(), autorService.enableStatus(id), String.format("%s/%d", Uri.AUTOR_ACTIVAR, id)));
+    }
+
+    private ResponseEntity<?> responseAutorDisable(int id) throws ExceptionBBDD {
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseInfo(HttpStatus.OK.value(), autorService.disableStatus(id), String.format("%s/%d", Uri.AUTOR_DESACTIVAR, id)));
+    }
+
+    private ResponseEntity<?> responseAutor(int id) throws ExceptionBBDD {
+        return ResponseEntity.status(HttpStatus.OK).body(modelMapperDTO.autorToDto(autorService.findById(id)));
+    }
+
+    private ResponseEntity<?> responseAutorUpdate(int id, AutorRequestDTO autor) throws ExceptionBBDD, ExceptionBadRequest {
+        return ResponseEntity.status(HttpStatus.OK).body(autorService.update(id, autor));
+    }
+
+    private ResponseEntity<?> responseExceptionBBDD(Exception exception, String path) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseInfo(HttpStatus.BAD_REQUEST.value(), exception.getMessage(), path));
+    }
+
+    private ResponseEntity<?> responseExceptionBadRequest(Exception exception, String path) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseInfo(HttpStatus.BAD_REQUEST.value(), exception.getMessage(), path));
     }
 }
