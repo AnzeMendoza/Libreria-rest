d114a831332822ba652f9b7d58c3173cfd57f357
Facundo Cordoba <fscordoba@gylgroup.com>
2022-03-10 16:58:41 -0300

    se  suben archivos asincronicos y constantes

---
+++ src/main/resources/pagina/js/constantes.js
@@ -0,0 +1,9 @@
+export const options = {method: '',
+							 body: '',
+               headers: {
+               'Content-type': 'application/json; charset=UTF-8',
+               },
+              }
+export const urlAutor =  "http://localhost:8080/api/v1/autor/"; 
+
+
